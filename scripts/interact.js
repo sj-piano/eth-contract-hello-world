c638949f4a576f7d6d5a16659230bbbe0cd554f5
StJohn Piano <stjohnpiano@gmail.com>
2023-06-17 17:16:53 +0200

    CONTRACT_ADDRESS -> DEPLOYED_CONTRACT_ADDRESS

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,6 +1,6 @@
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const DEPLOYED_CONTRACT_ADDRESS = process.env.DEPLOYED_CONTRACT_ADDRESS;
 
 const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
 
@@ -8,7 +8,7 @@ const infuraProvider = new ethers.providers.InfuraProvider(network="sepolia", IN
 
 const signer = new ethers.Wallet(PRIVATE_KEY, infuraProvider);
 
-const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+const helloWorldContract = new ethers.Contract(DEPLOYED_CONTRACT_ADDRESS, contract.abi, signer);
 
 async function main() {
   const message = await helloWorldContract.message();
