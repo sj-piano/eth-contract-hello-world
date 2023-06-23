2b4905b831cca73a064183b852b476a67239b279
StJohn Piano <stjohnpiano@gmail.com>
2023-06-23 18:33:05 +0200

    Streamline scripts/deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,15 @@
 // Imports
 const { program } = require("commander");
 const { ethers } = require("ethers");
+const Joi = require("joi");
 const _ = require("lodash");
 
 // Local imports
-const utils = require("#root/lib/utils.js");
+const { config } = require("#root/config.js");
+const ethereum = require("#root/src/ethereum.js");
 
-// Settings
-const networkLabelList = "local testnet mainnet".split(" ");
+// Controls
+const initialMessage = "Hello World!";
 
 // Load environment variables
 require("dotenv").config();
@@ -37,41 +39,36 @@ let { debug, network: networkLabel } = options;
 
 // Process and validate arguments
 
-if (!networkLabelList.includes(networkLabel)) {
-  console.error(
-    `Invalid network "${networkLabel}". Valid options are: [${networkLabelList.join(
-      ", "
-    )}]`
-  );
+const networkLabelSchema = Joi.string().valid(...config.networkLabelList);
+let networkLabelResult = networkLabelSchema.validate(networkLabel);
+if (networkLabelResult.error) {
+  let msg = `Invalid network "${networkLabel}". Valid options are: [${config.networkLabelList.join(
+    ", "
+  )}]`;
+  console.error(msg);
   process.exit(1);
 }
-
-mapNetworkLabelToNetwork = {
-  local: "http://localhost:8545",
-  testnet: "sepolia",
-  mainnet: "mainnet",
-}
-let network = mapNetworkLabelToNetwork[networkLabel];
+const network = config.mapNetworkLabelToNetwork[networkLabel];
 
 // Setup
 
 const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
 
 let provider, signer, contractFactoryHelloWorld;
+
 let msg;
 if (networkLabel == "local") {
   msg = `Connecting to ${networkLabel} network at ${network}...`;
   provider = new ethers.JsonRpcProvider(network);
   signer = new ethers.Wallet(LOCAL_HARDHAT_PRIVATE_KEY, provider);
-  contractFactoryHelloWorld = new ethers.ContractFactory(contract.abi, contract.bytecode, signer);
 } else if (networkLabel == "testnet") {
   msg = `Connecting to ${networkLabel} ${network} network...`;
   provider = new ethers.InfuraProvider(network, INFURA_API_KEY);
   signer = new ethers.Wallet(TESTNET_SEPOLIA_PRIVATE_KEY, provider);
-  contractFactoryHelloWorld = new ethers.ContractFactory(contract.abi, contract.bytecode, signer);
 } else if (networkLabel == "mainnet") {
   throw new Error("Not implemented yet");
 }
+contractFactoryHelloWorld = new ethers.ContractFactory(contract.abi, contract.bytecode, signer);
 log(msg);
 
 // Run main function
@@ -88,7 +85,6 @@ main()
 async function main() {
 
   // Deploy contract
-  const initialMessage = "Hello World!";
   const contractHelloWorld = await contractFactoryHelloWorld.deploy(initialMessage);
   await contractHelloWorld.waitForDeployment();
   const txDeployment = contractHelloWorld.deploymentTransaction();
