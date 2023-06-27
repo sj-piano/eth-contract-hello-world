be91e67af6b951f7a19cb5de4cf5d90ad6a26b9c
StJohn Piano <stjohnpiano@gmail.com>
2023-06-27 19:43:07 +0200

    scripts/deploy.js: Logging & fee estimation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
 // Imports
+const Big = require("big.js");
 const { program } = require("commander");
 const { ethers } = require("ethers");
 const Joi = require("joi");
@@ -7,6 +8,7 @@ const _ = require("lodash");
 // Local imports
 const { config } = require("#root/config.js");
 const ethereum = require("#root/src/ethereum.js");
+const { createLogger } = require("#root/src/logging.js");
 
 // Controls
 const initialMessage = "Hello World!";
@@ -22,11 +24,12 @@ const {
 } = process.env;
 
 // Logging
-let log = console.log;
+const { logger, log, deb } = createLogger();
 
 // Parse arguments
 program
   .option("-d, --debug", "log debug information")
+  .option("--log-level <logLevel>", "Specify log level.", "error")
   .option(
     "--network <network>",
     "specify the Ethereum network to connect to",
@@ -35,14 +38,26 @@ program
 program.parse();
 const options = program.opts();
 if (options.debug) log(options);
-let { debug, network: networkLabel } = options;
+let { debug, logLevel, network: networkLabel } = options;
 
 // Process and validate arguments
+
+const logLevelSchema = Joi.string().valid(...config.logLevelList);
+let logLevelResult = logLevelSchema.validate(logLevel);
+if (logLevelResult.error) {
+  var msg = `Invalid log level "${logLevel}". Valid options are: [${config.logLevelList.join(", ")}]`;
+  console.error(msg);
+  process.exit(1);
+}
+if (debug) {
+  logLevel = "debug";
+}
+logger.setLevel({ logLevel });
 
 const networkLabelSchema = Joi.string().valid(...config.networkLabelList);
 let networkLabelResult = networkLabelSchema.validate(networkLabel);
 if (networkLabelResult.error) {
-  let msg = `Invalid network "${networkLabel}". Valid options are: [${config.networkLabelList.join(
+  var msg = `Invalid network "${networkLabel}". Valid options are: [${config.networkLabelList.join(
     ", "
   )}]`;
   console.error(msg);
@@ -54,9 +69,9 @@ const network = config.mapNetworkLabelToNetwork[networkLabel];
 
 const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
 
-let provider, signer, contractFactoryHelloWorld;
+let provider, signer;
 
-let msg;
+var msg;
 if (networkLabel == "local") {
   msg = `Connecting to ${networkLabel} network at ${network}...`;
   provider = new ethers.JsonRpcProvider(network);
@@ -68,7 +83,7 @@ if (networkLabel == "local") {
 } else if (networkLabel == "mainnet") {
   throw new Error("Not implemented yet");
 }
-contractFactoryHelloWorld = new ethers.ContractFactory(contract.abi, contract.bytecode, signer);
+let contractFactoryHelloWorld = new ethers.ContractFactory(contract.abi, contract.bytecode, signer);
 log(msg);
 
 // Run main function
@@ -84,13 +99,62 @@ main()
 
 async function main() {
 
-  // Deploy contract
-  const contractHelloWorld = await contractFactoryHelloWorld.deploy(initialMessage);
+  // Estimate fees.
+  // - Stop if any fee limit is exceeded.
+  const txRequest = await contractFactoryHelloWorld.getDeployTransaction(initialMessage);
+  const estimatedFees = await ethereum.estimateFees({ config, logger, provider, txRequest });
+  deb(estimatedFees);
+  const { gasLimit, maxFeePerGasWei, maxPriorityFeePerGasWei, feeEth, feeUsd, feeLimitChecks } = estimatedFees;
+  log(`Estimated fee: ${feeEth} ETH (${feeUsd} USD)`);
+  if (feeLimitChecks.anyLimitExceeded) {
+    for (let key of feeLimitChecks.limitExceededKeys) {
+      let check = feeLimitChecks[key];
+      console.error(`- ${key}: ${check.msg}`);
+    }
+    process.exit(1);
+  }
+  const gasPrices = estimatedFees.gasPrices;
+  const { ethToUsd } = gasPrices;
+
+  // Get ETH balance of signer.
+  // - Stop if balance is too low.
+  const signerAddress = await signer.getAddress();
+  const signerBalanceWei = await provider.getBalance(signerAddress);
+  const signerBalanceEth = ethers.formatEther(signerBalanceWei);
+  const signerBalanceUsd = (Big(ethToUsd).mul(Big(signerBalanceEth))).toFixed(config.USD_DP);
+  log(`Signer balance: ${signerBalanceEth} ETH (${signerBalanceUsd} USD)`);
+  if (Big(signerBalanceEth).lt(Big(feeEth))) {
+    console.error(`Signer balance is too low. Need at least ${feeEth} ETH.`);
+    process.exit(1);
+  }
+
+  // Deploy contract.
+  // - Use the estimated fee values.
+  // - Wait for deployment to complete.
+  const contractHelloWorld = await contractFactoryHelloWorld.deploy(initialMessage, {
+    gasLimit,
+    maxFeePerGas: maxFeePerGasWei,
+    maxPriorityFeePerGas: maxPriorityFeePerGasWei,
+  });
   await contractHelloWorld.waitForDeployment();
+
+  // Examine the results and find out how much was spent.
   const txDeployment = contractHelloWorld.deploymentTransaction();
-  //log(txDeployment)
+  deb(txDeployment);
   const txReceipt = await txDeployment.wait();
-  //log(txReceipt)
+  deb(txReceipt);
+  const { gasUsed, gasPrice: effectiveGasPrice } = txReceipt;
+  deb(`Gas used: ${gasUsed} wei`);
+  deb(`Effective gas price: ${effectiveGasPrice} wei`);
+
+  const txFeeWei = txReceipt.fee;
+  deb(`txFeeWei: ${txFeeWei}`);
+  const txFeeEth = ethers.formatEther(txFeeWei).toString();
+  const txFeeUsd = (Big(ethToUsd).mul(Big(txFeeEth))).toFixed(config.USD_DP);
+  log(`Final fee: ${txFeeEth} ETH (${txFeeUsd} USD)`);
+
+  // Report the final result.
   let contractAddress = contractHelloWorld.target;
-  log(`Contract deployed to address: ${contractAddress}`);
+  log(`Contract deployed to address:`);
+  console.log(contractAddress);
 }
