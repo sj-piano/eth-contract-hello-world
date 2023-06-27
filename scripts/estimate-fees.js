2739ebcf80c98eb25aec5cbca2e5ad2fffb7804f
StJohn Piano <stjohnpiano@gmail.com>
2023-06-27 19:41:01 +0200

    scripts/estimate-fees.js: Build out logging

--- scripts/estimate-fees.js
+++ scripts/estimate-fees.js
@@ -1,5 +1,3 @@
-// Analyse the current gas fees on a network.
-
 // Imports
 const { program } = require("commander");
 const { ethers } = require("ethers");
@@ -9,6 +7,7 @@ const _ = require("lodash");
 // Local imports
 const { config } = require("#root/config.js");
 const ethereum = require("#root/src/ethereum.js");
+const { createLogger } = require("#root/src/logging.js");
 
 // Load environment variables
 require("dotenv").config();
@@ -19,13 +18,12 @@ const {
 } = process.env;
 
 // Logging
-let log = console.log;
-let jj = (x) => JSON.stringify(x, null, 2);
-let lj = (x) => log(jj(x));
+const { logger, log, deb } = createLogger();
 
 // Parse arguments
 program
   .option("-d, --debug", "log debug information")
+  .option("--log-level <logLevel>", "Specify log level.", "error")
   .option(
     "--network <network>",
     "specify the Ethereum network to connect to",
@@ -34,14 +32,26 @@ program
 program.parse();
 const options = program.opts();
 if (options.debug) log(options);
-let { debug, network: networkLabel } = options;
+let { debug, logLevel, network: networkLabel } = options;
 
 // Process and validate arguments
+
+const logLevelSchema = Joi.string().valid(...config.logLevelList);
+let logLevelResult = logLevelSchema.validate(logLevel);
+if (logLevelResult.error) {
+  var msg = `Invalid log level "${logLevel}". Valid options are: [${config.logLevelList.join(", ")}]`;
+  console.error(msg);
+  process.exit(1);
+}
+if (debug) {
+  logLevel = "debug";
+}
+logger.setLevel({ logLevel });
 
 const networkLabelSchema = Joi.string().valid(...config.networkLabelList);
 let networkLabelResult = networkLabelSchema.validate(networkLabel);
 if (networkLabelResult.error) {
-  let msg = `Invalid network "${networkLabel}". Valid options are: [${config.networkLabelList.join(
+  var msg = `Invalid network "${networkLabel}". Valid options are: [${config.networkLabelList.join(
     ", "
   )}]`;
   console.error(msg);
@@ -53,8 +63,9 @@ const network = config.mapNetworkLabelToNetwork[networkLabel];
 
 const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
 
-let provider, signer, contractFactoryHelloWorld;
+let provider, signer;
 
+var msg;
 if (networkLabel == "local") {
   msg = `Connecting to ${networkLabel} network at ${network}...`;
   provider = new ethers.JsonRpcProvider(network);
@@ -65,8 +76,8 @@ if (networkLabel == "local") {
   provider = new ethers.InfuraProvider(network, INFURA_API_KEY);
   DEPLOYED_CONTRACT_ADDRESS = TESTNET_SEPOLIA_DEPLOYED_CONTRACT_ADDRESS;
 }
-contractFactoryHelloWorld = new ethers.ContractFactory(contract.abi, contract.bytecode, provider);
-contractHelloWorld = new ethers.Contract(
+const contractFactoryHelloWorld = new ethers.ContractFactory(contract.abi, contract.bytecode, provider);
+const contractHelloWorld = new ethers.Contract(
   DEPLOYED_CONTRACT_ADDRESS,
   contract.abi,
   signer
@@ -86,16 +97,23 @@ main()
 
 async function main() {
 
-  // Confirm connection
-  //let blockNumber = await provider.getBlockNumber();
-  //log(`Current block number: ${blockNumber}`);
+  let blockNumber = await provider.getBlockNumber();
+  deb(`Current block number: ${blockNumber}`);
 
   // Contract deployment
   const initialMessage = "Hello World!";
   const txRequest = await contractFactoryHelloWorld.getDeployTransaction(initialMessage);
   const estimatedFees = await ethereum.estimateFees({ config, provider, txRequest });
-  log(`Contract deployment - estimated fees:`);
-  lj(estimatedFees);
+  console.log(`Contract deployment - estimated fees:`);
+  log(estimatedFees);
+  console.log(`- feeEth: ${estimatedFees.feeEth}`);
+  console.log(`- feeUsd: ${estimatedFees.feeUsd}`);
+  if (estimatedFees.feeLimitChecks.anyLimitExceeded) {
+    for (let key of estimatedFees.feeLimitChecks.limitExceededKeys) {
+      let check = estimatedFees.feeLimitChecks[key];
+      console.log(`- ${key}: ${check.msg}`);
+    }
+  }
 
   // Check if contract exists at address
   let address = contractHelloWorld.target;
@@ -110,7 +128,15 @@ async function main() {
   const newMessage = "Hello World! Updated.";
   const txRequest2 = await contractHelloWorld.update.populateTransaction(newMessage);
   const estimatedFees2 = await ethereum.estimateFees({ config, provider, txRequest: txRequest2 });
-  log(`Contract method call: update - estimated fees:`);
-  lj(estimatedFees2);
+  console.log(`Contract method call: 'update' - estimated fees:`);
+  log(estimatedFees2);
+  console.log(`- feeEth: ${estimatedFees2.feeEth}`);
+  console.log(`- feeUsd: ${estimatedFees2.feeUsd}`);
+  if (estimatedFees2.feeLimitChecks.anyLimitExceeded) {
+    for (let key of estimatedFees2.feeLimitChecks.limitExceededKeys) {
+      let check = estimatedFees2.feeLimitChecks[key];
+      console.log(`- ${key}: ${check.msg}`);
+    }
+  }
 
 }
