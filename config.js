34ff83b647e917f9c2b061dd3e433365d0ede9fc
StJohn Piano <stjohnpiano@gmail.com>
2023-07-01 17:55:38 +0200

    Smarter config - handle setting fee limits

--- config.js
+++ config.js
@@ -3,6 +3,10 @@
 // Imports
 const { ethers } = require("ethers");
 const Joi = require("joi");
+const _ = require("lodash");
+
+// Local imports
+const { validateNumericString } = require("#root/lib/utils.js");
 
 /* Notes:
 - The main application or script will load the config, apply changes from cmdline arguments and environment variables if required, and pass it to the other modules or functions as an object.
@@ -12,13 +16,15 @@ const Joi = require("joi");
 class Config {
   constructor() {
     // Note: maxFeePerTransactionUsd overrides the other fee limits.
-    this.maxFeePerTransactionUsd = "5.00";
-    this.maxFeePerGasGwei = "100";
-    this.maxPriorityFeePerGasGwei = "2.0";
+    this.maxFeePerTransactionUsd = "0";
+    this.maxFeePerGasGwei = "0";
+    this.maxPriorityFeePerGasGwei = "0";
     this.gasLimitMultiplier = "1.0";
     this.averagePriorityFeeMultiplier = "1.5";
     this.eth_usd_price_url =
       "https://api.pro.coinbase.com/products/ETH-USD/ticker";
+    this.maxFeePerGasWei = "0";
+    this.maxPriorityFeePerGasWei = "0";
     this.networkLabelList = "local testnet mainnet".split(" ");
     this.mapNetworkLabelToNetwork = {
       local: "http://localhost:8545",
@@ -31,14 +37,42 @@ class Config {
     this.GWEI_DP = 9;
     this.ETH_DP = 18;
     this.USD_DP = 2;
-    // Derive more values.
-    this.maxFeePerGasWei = ethers.parseUnits(this.maxFeePerGasGwei, "gwei");
-    this.maxPriorityFeePerGasWei = ethers.parseUnits(
-      this.maxPriorityFeePerGasGwei,
-      "gwei"
-    );
     this.dummyAddress = "0x000000000000000000000000000000000000dEaD";
   }
+
+  // Setters
+
+  set maxFeePerGasGwei(newValue) {
+    this._maxFeePerGasGwei = newValue;
+    this.maxFeePerGasWei = ethers.parseUnits(newValue, "gwei").toString();
+  }
+
+  set maxPriorityFeePerGasGwei(newValue) {
+    this._maxPriorityFeePerGasGwei = newValue;
+    this.maxPriorityFeePerGasWei = ethers.parseUnits(
+      newValue,
+      "gwei"
+    ).toString();
+  }
+
+  // Methods
+
+  update({
+    MAX_FEE_PER_TRANSACTION_USD,
+    MAX_FEE_PER_GAS_GWEI,
+    MAX_PRIORITY_FEE_PER_GAS_GWEI,
+  }) {
+    if (!_.isNil(MAX_FEE_PER_GAS_GWEI)) {
+      this.maxFeePerTransactionUsd = validateNumericString({ name: 'MAX_FEE_PER_TRANSACTION_USD', value: MAX_FEE_PER_TRANSACTION_USD });
+    }
+    if (!_.isNil(MAX_FEE_PER_GAS_GWEI)) {
+      this.maxFeePerGasGwei = validateNumericString({ name: 'MAX_FEE_PER_GAS_GWEI', value: MAX_FEE_PER_GAS_GWEI });
+    }
+    if (!_.isNil(MAX_PRIORITY_FEE_PER_GAS_GWEI)) {
+      this.maxPriorityFeePerGasGwei = validateNumericString({ name: 'MAX_PRIORITY_FEE_PER_GAS_GWEI', value: MAX_PRIORITY_FEE_PER_GAS_GWEI });
+    }
+  }
+
 }
 
 async function validateConfig({ config }) {
@@ -49,6 +83,7 @@ async function validateConfig({ config }) {
     console.error(`config: ${config}`);
     throw new Error(`Invalid config: ${result.error}`);
   }
+  return config;
 }
 
 module.exports = {
