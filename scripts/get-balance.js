5fb4b0bf4747370680be65a9c4291aac916943d8
StJohn Piano <stjohnpiano@gmail.com>
2023-07-01 20:26:09 +0200

    scripts/get-balance.js

---
+++ scripts/get-balance.js
@@ -0,0 +1,111 @@
+// Imports
+const { program } = require("commander");
+const { ethers } = require("ethers");
+const fs = require("fs");
+const Joi = require("joi");
+const _ = require("lodash");
+
+// Local imports
+const { config } = require("#root/config.js");
+const ethereum = require("#root/src/ethereum.js");
+const { createLogger } = require("#root/lib/logging.js");
+
+// Load environment variables
+require("dotenv").config();
+const { INFURA_API_KEY } = process.env;
+
+// Logging
+const { logger, log, deb } = createLogger();
+
+// Parse arguments
+program
+  .option("-d, --debug", "log debug information")
+  .option("--log-level <logLevel>", "Specify log level.", "error")
+  .option(
+    "--network <network>",
+    "specify the Ethereum network to connect to",
+    "local"
+  )
+  .option(
+    "--address-file <addressFile>",
+    "Path to file containing Ethereum address."
+  );
+program.parse();
+const options = program.opts();
+if (options.debug) console.log(options);
+let { debug, logLevel, network: networkLabel, addressFile } = options;
+
+// Process and validate arguments
+
+const logLevelSchema = Joi.string().valid(...config.logLevelList);
+let logLevelResult = logLevelSchema.validate(logLevel);
+if (logLevelResult.error) {
+  var msg = `Invalid log level "${logLevel}". Valid options are: [${config.logLevelList.join(
+    ", "
+  )}]`;
+  console.error(msg);
+  process.exit(1);
+}
+if (debug) {
+  logLevel = "debug";
+}
+logger.setLevel({ logLevel });
+
+const networkLabelSchema = Joi.string().valid(...config.networkLabelList);
+let networkLabelResult = networkLabelSchema.validate(networkLabel);
+if (networkLabelResult.error) {
+  var msg = `Invalid network "${networkLabel}". Valid options are: [${config.networkLabelList.join(
+    ", "
+  )}]`;
+  console.error(msg);
+  process.exit(1);
+}
+const network = config.mapNetworkLabelToNetwork[networkLabel];
+
+let address;
+if (fs.existsSync(addressFile)) {
+  address = fs.readFileSync(addressFile).toString().trim();
+  deb(`Address found in ${addressFile}: ${address}`);
+}
+if (!ethers.isAddress(address)) {
+  var msg = `Invalid Ethereum address "${address}"`;
+  console.error(msg);
+  process.exit(1);
+}
+
+// Setup
+
+let provider;
+
+var msg;
+if (networkLabel == "local") {
+  msg = `Connecting to local network at ${network}...`;
+  provider = new ethers.JsonRpcProvider(network);
+} else if (networkLabel == "testnet") {
+  msg = `Connecting to Sepolia testnet...`;
+  provider = new ethers.InfuraProvider(network, INFURA_API_KEY);
+} else if (networkLabel == "mainnet") {
+  msg = `Connecting to Ethereum mainnet...`;
+  provider = new ethers.InfuraProvider(network, INFURA_API_KEY);
+}
+log(msg);
+
+// Run main function
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+// Functions
+
+async function main() {
+  let blockNumber = await provider.getBlockNumber();
+  deb(`Current block number: ${blockNumber}`);
+
+  let balanceEth = await provider.getBalance(address);
+  log(`Balance (ETH):`);
+  console.log(balanceEth);
+}
